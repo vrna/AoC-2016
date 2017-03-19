2be05539d635e7897c6788336033733e1bb6bf8b
vrna <artturi.koskela@gmail.com>
2017-03-19 21:37:49 +0200

    part 20. IRange numeric limit found...

--- AoC_20/ranger.R
+++ AoC_20/ranger.R
@@ -10,27 +10,31 @@ library(data.table)
 
 inputdata <- read.table(input,sep="-")
 sorted <- data.table(inputdata,key="V1")
-#print (sorted)
 smallest <- valid[2]
+sectionStart <- 0
+section <- valid
+unblocked <- 0
 for(i in 1:(dim(sorted)[1] - 1) ) {
-  end <- sorted[i,2]
-  start <- sorted[i+1,1]
-  if( end < start) {
-    # TODO: replace only if smaller than previous
-    if(end+1 < smallest)
+  startOfCurrent <- sorted[i,1]
+  endOfCurrent <- sorted[i,2]
+  startOfNext <- sorted[i+1,1]
+
+  if( startOfCurrent > section[1] ) {
+    sectionLength <- startOfCurrent - section[1]
+    unblocked <- unblocked + sectionLength
+    if( section[1] < smallest)
     {
-      smallest <- end + 1
+      smallest <- section[[1]]
     }
   }
 
-  # TODO: check that smallest is not in the range
-  if(smallest >= sorted[i,1] && smallest <= sorted[i,2] )
-  {
-    smallest <- valid[2]
+  if( endOfCurrent > section[1]) {
+    section[1] <- endOfCurrent + 1
   }
 }
-print ("smallest:") # 17 348 574 first ry
-print (smallest)
+print(paste("unblocked: ",unblocked))
+print (paste("smallest:",smallest)) # 17348574 first ry
+
 end.time <- Sys.time()
 time.taken <- end.time - start.time
 print (time.taken)
